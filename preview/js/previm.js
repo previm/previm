30a005f89f9020c0f420e71774c305d2e1732d68
masataka kanno <akapanna@gmail.com>
2013-06-16 19:38:27 +0900

    フォーマット修正、typo修正

--- preview/js/previm.js
+++ preview/js/previm.js
@@ -4,30 +4,30 @@
 
   function loadPreview() {
     if (typeof getFileName === 'function') {
-	  _doc.getElementById("markdown-file-name").innerHTML = getFileName();
+      _doc.getElementById("markdown-file-name").innerHTML = getFileName();
     }
     if (typeof getLastModified === 'function') {
-	  _doc.getElementById("last-modified").innerHTML = getLastModified();
+      _doc.getElementById("last-modified").innerHTML = getLastModified();
     }
     if (typeof getContent === 'function') {
-	  _doc.getElementById("preview").innerHTML = converter.makeHtml(getContent());
+      _doc.getElementById("preview").innerHTML = converter.makeHtml(getContent());
     }
   }
 
-  _win.setInterval(function(){
-	var script = _doc.createElement("script");
+  _win.setInterval(function() {
+    var script = _doc.createElement("script");
 
-	script.type = 'text/javascript';
-	script.src = 'js/previm-function.js?t=' + new Date().getTime();
+    script.type = 'text/javascript';
+    script.src = 'js/previm-function.js?t=' + new Date().getTime();
 
-	script.addEventListener("load", function(){
-		loadPreview();
-		_wiin.setTimeout( function(){
-			script.parentNode.removeChild(script);
-		}, 160 );
-	}, false );
+    script.addEventListener("load", function() {
+      loadPreview();
+      _win.setTimeout(function() {
+        script.parentNode.removeChild(script);
+      }, 160);
+    }, false);
 
-	_doc.getElementsByTagName("head")[0].appendChild(script);
+    _doc.getElementsByTagName("head")[0].appendChild(script);
 
   }, REFRESH_INTERVAL);
 
