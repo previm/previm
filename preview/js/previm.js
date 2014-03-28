3017b15b0925861a118497163a978b83f7197a9a
m-kanno <akapanna@gmail.com>
2014-03-29 08:38:59 +0900

    ダブルクオート統一したりロジックに影響しない修正

--- preview/js/previm.js
+++ preview/js/previm.js
@@ -1,46 +1,48 @@
+'use strict';
+
 (function(_doc, _win) {
   var REFRESH_INTERVAL = 1000;
 
   function loadPreview() {
-    var need_reload = false;
+    var needReload = false;
     // These functions are defined as the file generated dynamically.
     //   generator-file: preview/autoload/previm.vim
     //   generated-file: preview/js/previm-function.js
     if (typeof getFileName === 'function') {
-      if (_doc.getElementById("markdown-file-name").innerHTML != getFileName()) {
-        _doc.getElementById("markdown-file-name").innerHTML = getFileName();
-        need_reload = true;
+      if (_doc.getElementById('markdown-file-name').innerHTML !== getFileName()) {
+        _doc.getElementById('markdown-file-name').innerHTML = getFileName();
+        needReload = true;
       }
     } else {
-      need_reload = true;
+      needReload = true;
     }
     if (typeof getLastModified === 'function') {
-      if (_doc.getElementById("last-modified").innerHTML != getLastModified()) {
-        _doc.getElementById("last-modified").innerHTML = getLastModified();
-        need_reload = true;
+      if (_doc.getElementById('last-modified').innerHTML !== getLastModified()) {
+        _doc.getElementById('last-modified').innerHTML = getLastModified();
+        needReload = true;
       }
     } else {
-      need_reload = true;
+      needReload = true;
     }
-    if (need_reload && (typeof getContent === 'function')) {
-      _doc.getElementById("preview").innerHTML = marked(getContent());
+    if (needReload && (typeof getContent === 'function')) {
+      _doc.getElementById('preview').innerHTML = marked(getContent());
     }
   }
 
   _win.setInterval(function() {
-    var script = _doc.createElement("script");
+    var script = _doc.createElement('script');
 
     script.type = 'text/javascript';
     script.src = 'js/previm-function.js?t=' + new Date().getTime();
 
-    _addEventListener(script, "load", (function() {
+    _addEventListener(script, 'load', (function() {
       loadPreview();
       _win.setTimeout(function() {
         script.parentNode.removeChild(script);
       }, 160);
     })());
 
-    _doc.getElementsByTagName("head")[0].appendChild(script);
+    _doc.getElementsByTagName('head')[0].appendChild(script);
 
   }, REFRESH_INTERVAL);
 
